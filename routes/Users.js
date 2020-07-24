6ed8bd2e51296035aacf9f17e3c5d8a2ff87f717
sharmakumaraditya <sharmakumaraditya@gmail.com>
2020-07-25 03:11:01 +0530

    Added Authentication Feature

--- routes/Users.js
+++ routes/Users.js
@@ -19,9 +19,10 @@ process.env.SECRET_KEY = 'secret'
 process.env.CLIENT_URL = 'accountactivatekey123'
 process.env.CLIENT_URL = 'http://localhost:3000'
 
+
 users.post('/register', (req, res) => {
     const today = new Date()
-    const userData = {
+    const payload = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
@@ -36,15 +37,36 @@ users.post('/register', (req, res) => {
         if(!user)
         {
             bcrypt.hash(req.body.password, 10, (err, hash) => {
-                userData.password = hash
-                User.create(userData)
-                .then(user => {
-                    res.json({status: user.email + 'registered!'})
-                })
-                .catch(err => {
-                    res.send('error: ' + err)
-                })
+                payload.password = hash
             })
+            const token = jwt.sign(payload, process.env.SECRET_KEY, {
+                expiresIn: 1440
+              });
+
+            let mailTransporter = nodemailer.createTransport({ 
+                service: 'gmail', 
+                auth: { 
+                    user: 'compscncec@gmail.com', 
+                    pass: '4321csec4321'
+                } 
+            });
+            const data = {
+              from : 'csecgroup2017@gmail.com',
+              to : payload.email,
+              subject : 'PASSWORD RESET LINK',
+              html : `
+                  <h3> Click here to Activate Your Account</h3>
+                  <link><p>${process.env.CLIENT_URL}/users/authenticate/${token}</p></link>
+              `
+            };
+            mailTransporter.sendMail(data, function(error, data) { 
+                if(error){
+                    return res.json({
+                      error : error.message
+                    })
+                  }
+                  return res.status(400).json({error : "Authentication mail has been sent successfully!"})
+                })
         }
         else{
             res.json({error: 'User already exists'})
@@ -54,6 +76,65 @@ users.post('/register', (req, res) => {
         res.send('error: ' + err)
     })
 })
+
+
+users.post('/authenticate',(req,res)=>{
+
+    if(req.body.token){
+          jwt.verify(req.body.token,process.env.SECRET_KEY,function(error,decodedData)
+          {
+            if(error){
+              return res.status(401).json({
+                error : "Incorrect or Expired Link"
+              })
+            }
+            const {first_name, last_name, email, password, created}=decodedData;
+            const temp_email = email
+            User.findOne({
+               email : temp_email
+              })
+              .then(user => 
+                {
+                if(!user)
+                {
+                    const userData = {
+                        first_name: first_name,
+                        last_name: last_name,
+                        email: email,
+                        password: password,
+                        created: created
+                    }
+                    bcrypt.hash(password, 10, (err, hash) => {
+                        userData.password = hash
+                        User.create(userData)
+                       .then(user => {
+                            res.json({status: user.email + ' registered!'})
+                        })
+                        .catch(err => {
+                            res.send('error: ' + err)
+                        })
+                    })
+    
+                }
+                else {
+            
+                    res.status(400).json({error : "User Already Exist"})
+                  }
+                })
+                .catch(err => {
+                    res.send('error: ' + err)
+                  })
+            })
+    
+        }
+        else{
+            return res.status(401).json({error : "Authentication Error"})
+        }
+      
+    })
+
+
+
 
 users.post('/login', (req, res) => {
     User.findOne({
@@ -109,8 +190,6 @@ users.post('/login', (req, res) => {
 
 
 
-
-
   users.put('/forgot-password',(req,res)=>{
     User.findOne({
         email: req.body.email
@@ -126,7 +205,7 @@ users.post('/login', (req, res) => {
             }
                 
                 const token = jwt.sign(payload, process.env.SECRET_KEY, {
-                    expiresIn: 1440
+                    expiresIn: 3000
                   });
 
                 let mailTransporter = nodemailer.createTransport({ 
@@ -142,7 +221,7 @@ users.post('/login', (req, res) => {
                   subject : 'PASSWORD RESET LINK',
                   html : `
                       <h3> Click here to reset the password!</h3>
-                      <a>${process.env.CLIENT_URL}/resetpassword/${token}</a>
+                      <p><link>${process.env.CLIENT_URL}/users/resetpassword/${token}</p></link>
                   `
                 };
                 return user.updateOne({resetLink: token}, function(err,success) {
@@ -171,8 +250,103 @@ users.post('/login', (req, res) => {
               res.send('error: ' + err)
             })
         })
+            
 
 
+
+users.put('/resetpassword',(req,res)=>{
+
+if(req.body.resetLink){
+      jwt.verify(req.body.resetLink,process.env.SECRET_KEY,function(error,decodedData)
+      {
+        if(error){
+          return res.status(401).json({
+            error : "Token expired"
+          })
+        }
+        User.findOne({
+            resetLink: req.body.resetLink
+          })
+          .then(user => 
+            {
+            if(user)
+            {
+
+                  var temp_password
+                  bcrypt.hash(req.body.newPass, 10, (err, hash) => {
+                    const obj = {
+                        password : hash,
+                        resetLink : ''
+                      }
+            
+                      user = _.extend(user,obj)
+                      user.save((err,result) =>{
+                        if(err){
+                          return res.status(400).json({error : "Password error"})
+                        }
+                        else{
+                          
+                            return res.status(200).json({message : "password changed"})                    
+                        }          
+                      })
+                  })
+
+            }
+            else {
+        
+                res.status(400).json({error : "User with this token not exist"})
+              }
+            })
+            .catch(err => {
+                res.send('error: ' + err)
+              })
+        })
+
+    }
+    else{
+        return res.status(401).json({error : "Authentication Error"})
+    }
+  
+})
+
+
+
+
+// users.post('/register', (req, res) => {
+//     const today = new Date()
+//     const userData = {
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         email: req.body.email,
+//         password: req.body.password,
+//         created: today
+//     }
+
+//     User.findOne({
+//         email: req.body.email
+//     })
+//     .then(user => {
+//         if(!user)
+//         {
+//             bcrypt.hash(req.body.password, 10, (err, hash) => {
+//                 userData.password = hash
+//                 User.create(userData)
+//                 .then(user => {
+//                     res.json({status: user.email + 'registered!'})
+//                 })
+//                 .catch(err => {
+//                     res.send('error: ' + err)
+//                 })
+//             })
+//         }
+//         else{
+//             res.json({error: 'User already exists'})
+//         }
+//     })
+//     .catch(err => {
+//         res.send('error: ' + err)
+//     })
+// })
 
 
 
@@ -228,95 +402,7 @@ users.post('/login', (req, res) => {
 //               res.send('error: ' + err)
 //             })
 //         })
-            
 
 
 
-users.put('/resetpassword',(req,res)=>{
-
-if(req.body.resetLink){
-      jwt.verify(req.body.resetLink,process.env.SECRET_KEY,function(error,decodedData)
-      {
-        if(error){
-          return res.status(401).json({
-            error : "Token expired"
-          })
-        }
-        User.findOne({
-            resetLink: req.body.resetLink
-          })
-          .then(user => 
-            {
-            if(user)
-            {
-                const obj = {
-                    password : req.body.newPass
-                  }
-                  bcrypt.hash(req.body.newPass, 10, (err, hash) => {
-                    obj.password = hash
-                  })
-        
-                  user = _.extend(user,obj)
-                  user.save((err,result) =>{
-                    if(err){
-                      return res.status(400).json({error : "Passoword error "})
-                    }
-                    else{
-                      
-                        return res.status(200).json({message : "password changed"})                    
-                    }          
-                  })
-            }
-            else {
-        
-                res.status(400).json({error : "User with this token not exist"})
-              }
-            })
-            .catch(err => {
-                res.send('error: ' + err)
-              })
-        })
-
-    }
-    else{
-        return res.status(401).json({error : "Authentication Error"})
-    }
-  
-})
-
-
-
-
-
-//         User.findOne({resetLink},(err,user)=>{
-//           if(err || !user){
-//             return res.status(400).json({error : "User with this token not exist"})
-//           }
-//           const obj = {
-//             password : newPass
-//           }
-
-//           user = _.extend(user,obj)
-//           user.save((err,result) =>
-//           {
-//             if(err){
-//               return res.status(400).json({error : "Passoword error "})
-//             }
-//             else{
-              
-//                 return res.status(200).json({message : "password changed"})
-            
-//             }
-  
-//           })
-//         })
-//       })
-// }
-// else{
-//   return res.status(401).json({error : "Authentication Error"})
-// }
-
-
-
-// }
 module.exports = users
